1738b4ad66685b162aae369b86a7cbd8cd66be13
Magadanov <magadanov03@mail.ru>
2025-04-27 22:43:18 +0500

    feat/zip: implement zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
 const decompress = async () => {
     // Write your code here 
+    const sourceFilePath = path.join(__dirname, 'files', 'archive.gz');
+    const destinationFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+    const readableStream = fs.createReadStream(sourceFilePath);
+    const gunzipStream = createGunzip();
+    const writableStream = fs.createWriteStream(destinationFilePath);
+
+    readableStream
+        .pipe(gunzipStream)
+        .pipe(writableStream)
+        .on('finish', () => {
+            console.log('File successfully decompressed!');
+        });
+
+    readableStream.on('error', () => {
+        console.error('Something went wrong');
+    });
+
+    gunzipStream.on('error', () => {
+        console.error('Something went wrong');
+    });
+    writableStream.on('error', () => {
+        console.error('Something went wrong');
+    });
 };
 
 await decompress();
